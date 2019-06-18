ea1c121f3c5a8a29af961b4c357fc2c22828bd18
Leon Knauer <mail@leonknauer.com>
2019-06-18 02:30:20 +0200

    Init of dotfile folder

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias src="source ~/.zshrc"
+
+alias newalias="vim ~/.zsh/aliases.zsh && src"
+alias newfunction="vim ~/.zsh/functions.zsh && src"
+alias newvariable="vim ~/.zsh/variables.zsh && src"
---
+++ zsh/.zsh/variables.zsh
@@ -0,0 +1,5 @@
+export DOTPATH=$HOME/dotfiles
+export CACHE=$HOME/cache
+
+export HISTFILE=$CACHE/.zsh_history
+export ZSH_COMPDUMP="$CACHE/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
